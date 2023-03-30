678dcdfe65b22112e7ffeb9f6d089d0662180e33
Md Imon Hossain <imoncoc0@gmail.com>
2023-03-30 23:03:15 +0600

    data.json added with data

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SingleBlog from '../SingleBlog/SingleBlog';
 
 const Home = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetch("data.json")
+        .then((res) => res.json())
+        .then((data)=> setBlogs(data))
+        .catch((error) => console.log(error))
+    })
     return (
-        <div>
-            I am From Home
+        <div className='blog-container'>
+            {
+                blogs.map((blog)=><SingleBlog blog={blog}></SingleBlog>)
+            }
         </div>
     );
 };
---
+++ src/components/SingleBlog/SingleBlog.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleBlog = () => {
+    return (
+        <div>
+            <h2>I am from Single Blog</h2>
+        </div>
+    );
+};
+
+export default SingleBlog;
