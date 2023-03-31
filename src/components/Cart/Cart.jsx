6ce0e706ede49be6b6761835b7b6f1da397a771e
Md Imon Hossain <imoncoc0@gmail.com>
2023-03-31 10:12:56 +0600

    cart text and css added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = () => {
     return (
-        <div className='bg-warning'>
-            <h2>I am From Cart</h2>
+      <div className="ms-3">
+        <div className="spent-time text-center">
+          <p className="p-3 spent-time-text">Spent time on read: 10 min</p>
         </div>
+
+        <div className="book-mark mt-3 pb-1">
+          <p className='book-mark-text pt-3 ps-3'>BookMarked Blog: 18</p>
+          <div className='book-mark-item m-3 p-2'>
+            <p className='book-mark-item-text'>Master Microsoft Power Platform and Become an In-Demand!</p>
+          </div>
+        </div>
+      </div>
     );
 };
 
